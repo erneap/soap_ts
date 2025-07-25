ba64c25a703a0dc27e4c45ec32e7b3edec879131
Anton Erne <antonerne@yahoo.com>
2025-07-25 17:49:26 +0900

    Jul25-1749

--- node-ts-api/dist/index.js
+++ node-ts-api/dist/index.js
@@ -3,20 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
-const bookRoutes_1 = __importDefault(require("./routes/bookRoutes"));
-const translationRoutes_1 = __importDefault(require("./routes/translationRoutes"));
-const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
-if (process.env.MONGO_URI === undefined) {
-    dotenv_1.default.config();
-}
-const app = (0, express_1.default)();
+const app_1 = __importDefault(require("./app"));
 const PORT = process.env.PORT || 3000;
-app.use(express_1.default.json());
-app.use("/api", bookRoutes_1.default);
-app.use("/api", translationRoutes_1.default);
-app.use("/api", userRoutes_1.default);
-app.listen(PORT, () => {
+app_1.default.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
