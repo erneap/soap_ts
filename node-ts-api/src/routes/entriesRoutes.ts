ac7b980e6901888878a3773b8225206a3f2a2c07
Anton Erne <antonerne@yahoo.com>
2025-07-30 17:20:26 +0900

    Jul30-1720

--- node-ts-api/src/routes/entriesRoutes.ts
+++ node-ts-api/src/routes/entriesRoutes.ts
@@ -1,7 +1,63 @@
 import { Router, Request, Response } from "express";
 import { collections } from "../config/mongoconnect";
 import { Collection, ObjectId } from "mongodb";
+import { NewEntryRequest } from 'soap-models/dist/entries';
 
 const router = Router();
+
+router.get('/entries/year/:user/:year', async (req: Request, res: Response) => {
+  const entryCol: Collection | undefined = collections.entries;
+  if (entryCol) {
+
+  } else {
+    return res.status(404).send('No Soap Entry Collection');
+  }
+});
+
+router.get('/entries/dates/:user/:start/:end', async (req: Request, res: Response) => {
+  const entryCol: Collection | undefined = collections.entries;
+  if (entryCol) {
+
+  } else {
+    return res.status(404).send('No Soap Entry Collection');
+  }
+});
+
+router.get('/entry/:user/:date', async (req: Request, res: Response) => {
+  const entryCol: Collection | undefined = collections.entries;
+  if (entryCol) {
+
+  } else {
+    return res.status(404).send('No Soap Entry Collection');
+  }
+});
+
+router.post('/entry', async (req: Request, res: Response) => {
+  const entryCol: Collection | undefined = collections.entries;
+  if (entryCol) {
+    const data = req.body as NewEntryRequest;
+
+  } else {
+    return res.status(404).send('No Soap Entry Collection');
+  }
+});
+
+router.put('/entry', async (req: Request, res: Response) => {
+  const entryCol: Collection | undefined = collections.entries;
+  if (entryCol) {
+
+  } else {
+    return res.status(404).send('No Soap Entry Collection');
+  }
+});
+
+router.delete('/entry/:user/:date', async (req: Request, res: Response) => {
+  const entryCol: Collection | undefined = collections.entries;
+  if (entryCol) {
+
+  } else {
+    return res.status(404).send('No Soap Entry Collection');
+  }
+});
 
 export default router;
