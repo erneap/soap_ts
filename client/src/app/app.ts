98ab3124e2598676f64dc94d6045d85a37e1dd37
Anton Erne <antonerne@yahoo.com>
2025-07-31 18:48:30 +0900

    Jul31-1848

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductList } from './product-list/product-list';
+import { Home } from './home/home';
+import { MatToolbarModule, MatToolbar } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductList],
+  imports: [RouterOutlet, Home, MatToolbar, MatIcon],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
